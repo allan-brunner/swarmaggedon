413684ba0c3cd4fca07a0fc2197c8687f07cbea1
Allan Brunner <allan@allanbrunner.dev>
2026-09-10 15:31:58 +0200

    Prevent rerolling on weapon selection window

--- frontend/src/pages/Game.jsx
+++ frontend/src/pages/Game.jsx
@@ -448,7 +448,7 @@ function Game() {
               ))}
             </div>
 
-            {hud.rerollsLeft > 0 && hud.wave > 0 && (
+            {choice.type !== CHOICE_TYPE.WEAPON && hud.rerollsLeft > 0 && hud.wave > 0 && (
               <div style={{ marginTop: '30px', textAlign: 'center' }}>
                 <button className="menu-btn" onClick={() => engineRef.current.rerollChoice()}>
                   Reroll Choices ({hud.rerollsLeft} left)
